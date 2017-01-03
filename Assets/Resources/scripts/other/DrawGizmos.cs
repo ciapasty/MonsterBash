0972daab14c4022eea4a24426e8025af2d36d3e0
Maciej Eichler <maciej.eichler@gmail.com>
2017-01-03 14:14:42 +0100

    Minor fixes
      * Adjust player soul collider size
      * Add layers for collisions
      * Remove collisions between enemies and pickups and souls

--- Assets/Resources/scripts/other/DrawGizmos.cs
+++ Assets/Resources/scripts/other/DrawGizmos.cs
@@ -3,19 +3,19 @@ using System.Collections;
 
 public class DrawGizmos : MonoBehaviour {
 	
-//	void OnDrawGizmos() {
-//		// Attack range gizmo
-//		UnityEditor.Handles.color = Color.red;
-//		if (GetComponent<PlayerController>()) {
-//			UnityEditor.Handles.DrawWireDisc(GetComponent<Renderer>().bounds.center, Vector3.back, GetComponent<PlayerController>().attackRadius);
-//		} else if (GetComponent<EnemyController>()) {
-//			Vector3 attkCircleCenter = GetComponent<Renderer>().bounds.center;
-//			attkCircleCenter.y -= 0.1f;
-//			UnityEditor.Handles.DrawWireDisc(attkCircleCenter, Vector3.back, GetComponent<EnemyController>().attackRadius);
-//		}
-//
-//		// Collider bod gizmo
-//		UnityEditor.Handles.color = Color.yellow;
-//		UnityEditor.Handles.DrawWireCube(GetComponent<BoxCollider2D>().bounds.center, GetComponent<BoxCollider2D>().bounds.size);
-//	}
+	void OnDrawGizmos() {
+		// Attack range gizmo
+		UnityEditor.Handles.color = Color.red;
+		if (GetComponent<PlayerController>()) {
+			UnityEditor.Handles.DrawWireDisc(GetComponent<Renderer>().bounds.center, Vector3.back, GetComponent<PlayerController>().attackRadius);
+		} else if (GetComponent<EnemyController>()) {
+			Vector3 attkCircleCenter = GetComponent<Renderer>().bounds.center;
+			attkCircleCenter.y -= 0.1f;
+			UnityEditor.Handles.DrawWireDisc(attkCircleCenter, Vector3.back, GetComponent<EnemyController>().attackRadius);
+		}
+
+		// Collider bod gizmo
+		UnityEditor.Handles.color = Color.yellow;
+		UnityEditor.Handles.DrawWireCube(GetComponent<BoxCollider2D>().bounds.center, GetComponent<BoxCollider2D>().bounds.size);
+	}
 }
